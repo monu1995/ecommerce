fe8a788263cf6b8733fb8067c265c17ea35315ca
Manoranjan kumar <manoranjank@zignuts.com>
2020-03-30 11:58:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomePage from './component/Homepage'
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
