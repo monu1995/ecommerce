15e20e851e8506224b90cad1d6668a6211c916bb
Manoranjan kumar <manoranjank@zignuts.com>
2020-03-30 14:24:04 +0530

    add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import HomePage from './pages/Homepage'
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import Homepage from "./pages/Homepage";
+
+const Hats = (props) => {
+  console.log(props);
+  
+  return(
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  )
+};
 
 function App() {
   return (
-    <div >
-      <HomePage/>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop/hats" component={Hats} />
+      </Switch>
     </div>
   );
 }
